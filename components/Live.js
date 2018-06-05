ee7107df3d0fbfb81b476e3f09c0a2ceb8e68062
Rachel Lee <rachel@hedgeable.com>
2018-06-05 17:22:12 -0400

    update dependencies

--- components/Live.js
+++ components/Live.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 import { purple, white } from '../utils/colors'
+import { Permissions } from 'expo'
 
 export default class Live extends Component {
   state = {
